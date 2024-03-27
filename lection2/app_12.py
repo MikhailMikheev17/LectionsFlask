39f90c88202f42173d0debb1820f426cf57ca414
Mikhail <mikhail-mikheev-7@yandex.ru>
2024-03-27 17:52:18 +0300

    add app_12.py

---
+++ lection2/app_12.py
@@ -0,0 +1,29 @@
+from flask import Flask, flash, redirect, render_template,request, url_for
+
+app = Flask(__name__)
+
+app.secret_key = b'32afa21381144811307a9791a911003d3cfd36415a809e8f58256555ffb79d22'
+
+"""
+Генерация надежного секретного ключа
+>>> import secrets
+>>> secrets.token_hex()
+"""
+
+
+@app.route('/')
+def index():
+    return 'Hi'
+
+@app.route('/form/', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+    # Обработка данных формы
+        flash('Форма успешно отправлена!', 'success')
+        return redirect(url_for('form'))
+    return render_template('flash_form.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    #app.run(debug=False) 
